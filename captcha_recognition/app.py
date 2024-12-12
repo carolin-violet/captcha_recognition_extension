40ed9a0dfd7e2c3b91f493fb6fd3a75c0261f6b3
carolin-violet <2976008778@qq.com>
2024-12-12 23:49:55 +0800

    [feat] 增加前后端交互逻辑

--- captcha_recognition/app.py
+++ captcha_recognition/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from utils.orc_util import recognize
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/')
--- chrome_extension/content-script.js
+++ chrome_extension/content-script.js
@@ -5,9 +5,48 @@
 function getImg() {
   const canvasContainer = document.querySelector(".identityCode");
   const canvas = canvasContainer.querySelector("canvas");
-  console.log("canvas", canvas);
+  return new Promise((resolve, reject) => {
+    canvas.toBlob(
+      (blob) => {
+        if (blob) {
+          resolve(blob);
+        } else {
+          reject(new Error("Failed to create Blob"));
+        }
+      },
+      "image/png",
+      1
+    );
+  });
+}
+
+function getCaptchaCode(imgBlob) {
+  const url = "http://127.0.0.1:5000/img_recognition"; // 替换为你的 API 地址
+
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", url);
+  xhr.onload = function () {
+    if (xhr.status >= 200 && xhr.status < 300) {
+      const data = JSON.parse(xhr.responseText);
+      // 处理接收到的数据
+      console.log("数据:", data);
+    } else {
+      console.error("请求失败:", xhr.status);
+    }
+  };
+  xhr.onerror = function () {
+    console.error("请求错误");
+    document.getElementById("response").textContent = "请求错误";
+  };
+  xhr.send(imgBlob);
+}
+
+function main() {
+  getImg().then((img) => {
+    getCaptchaCode(img);
+  });
 }
 
 setTimeout(() => {
-  getImg();
+  main();
 }, 5000);
