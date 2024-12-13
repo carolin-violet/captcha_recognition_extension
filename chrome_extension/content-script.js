5afe3f1c89d25d340769bab7ed4a06c3a2eaab2a
carolin-violet <2976008778@qq.com>
2024-12-13 14:58:58 +0800

    [style] 样式修改

--- chrome_extension/content-script.js
+++ chrome_extension/content-script.js
@@ -1,3 +1,9 @@
+const userInfoObj = {
+  account: "violet",
+  password: "Trendy@123",
+  code: "",
+};
+
 // 获取canvas图片内容
 function getImg() {
   const canvasContainer = document.querySelector(".identityCode");
@@ -17,42 +23,29 @@ function getImg() {
   });
 }
 
-// 向input输入框中写入验证码
-function writeCode(code) {
-  const inputList = document.querySelectorAll(".el-input__inner");
-  const captchaInput = inputList[2];
-  captchaInput.value = code;
-}
-
-// 向input输入框中写入用户名密码
-function writeUserInfo() {
-  const inputList = document.querySelectorAll(".el-input__inner");
-  const accountInput = inputList[0];
-  const inputInput = inputList[1];
-  accountInput.value = "violet";
-  inputInput.value = "Trendy@123";
-}
-
 // api调用
 function getCaptchaCode(formData) {
   const url = "http://127.0.0.1:5000/img_recognition"; // 替换为你的 API 地址
 
-  const xhr = new XMLHttpRequest();
-  xhr.open("POST", url);
-  xhr.onload = function () {
-    if (xhr.status >= 200 && xhr.status < 300) {
-      // 处理接收到的数据
-      console.log("数据:", xhr.responseText);
-      writeCode(xhr.responseText);
-    } else {
-      console.error("请求失败:", xhr.status);
-    }
-  };
-  xhr.onerror = function () {
-    console.error("请求错误");
-    document.getElementById("response").textContent = "请求错误";
-  };
-  xhr.send(formData);
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", url);
+    xhr.onload = function () {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        // 处理接收到的数据
+        console.log("数据:", xhr.responseText);
+        resolve(xhr.responseText);
+      } else {
+        console.error("请求失败:", xhr.status);
+        reject(xhr.status);
+      }
+    };
+    xhr.onerror = function () {
+      console.error("请求错误");
+      document.getElementById("response").textContent = "请求错误";
+    };
+    xhr.send(formData);
+  });
 }
 
 function recognition() {
@@ -60,20 +53,65 @@ function recognition() {
     const file = new File([imgBlob], "image.png", { type: imgBlob.type });
     const formData = new FormData();
     formData.append("file", file);
-    getCaptchaCode(formData);
+    getCaptchaCode(formData).then((code) => {
+      userInfoObj.code = code;
+      // 自动填入用户信息
+      // writeUserInfo();
+
+      // 生成复制面板
+      generateDialog();
+    });
   });
 }
 
 // 监听html文档加载完成
 document.addEventListener("DOMContentLoaded", function () {
-  const recognition_button = document.createElement("div");
-  recognition_button.innerText = "填充";
-  recognition_button.className = "recognition_button";
-  document.body.appendChild(recognition_button);
-  recognition_button.onclick = function () {
-    // 填写用户名密码
-    writeUserInfo();
-    // 识别填写验证码
-    recognition();
-  };
+  const violet_recognition_button = document.createElement("div");
+  violet_recognition_button.innerText = "填充";
+  violet_recognition_button.className = "violet_recognition_button";
+  document.body.appendChild(violet_recognition_button);
+  violet_recognition_button.onclick = recognition;
 });
+
+// 自动向input输入框中写入用户名密码验证码
+function writeUserInfo() {
+  const inputList = document.querySelectorAll(".el-input__inner");
+  const accountInput = inputList[0];
+  const inputInput = inputList[1];
+  const captchaInput = inputList[2];
+  accountInput.value = userInfoObj.account;
+  inputInput.value = userInfoObj.password;
+  captchaInput.value = userInfoObj.code;
+}
+
+// 手动弹出复制粘贴弹窗
+function generateDialog() {
+  const copyBoard = document.createElement("div");
+  copyBoard.className = "violet_recognition_copy_board";
+  copyBoard.style.display = "block";
+
+  const accountContainer = document.createElement("div");
+  accountContainer.className = "copy-account";
+  accountContainer.innerText = userInfoObj.account;
+  copyBoard.appendChild(accountContainer);
+
+  const passwordContainer = document.createElement("div");
+  passwordContainer.className = "copy-password";
+  passwordContainer.innerText = userInfoObj.password;
+  copyBoard.appendChild(passwordContainer);
+
+  const codeContainer = document.createElement("div");
+  codeContainer.className = "copy-code";
+  codeContainer.innerText = userInfoObj.code;
+  copyBoard.appendChild(codeContainer);
+
+  const closeContainer = document.createElement("div");
+  closeContainer.className = "close";
+  closeContainer.innerText = "关闭";
+  copyBoard.appendChild(closeContainer);
+  closeContainer.onclick = function () {
+    copyBoard.style.display = "none";
+  };
+
+  document.body.appendChild(copyBoard);
+}
